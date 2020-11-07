3d6caa988466d141f652e3318a26f735332f5915
cqhaisenbao <fanyu9353@gmail.com>
2020-11-07 21:41:54 +0800

    创建icon组件

---
+++ src/components/icon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+// require('icons/money.svg');
+// require('icons/tag.svg');
+// require('icons/chart.svg');
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
+
+//声明属性的值的类型
+type Props = {
+    name: string
+}
+
+const Icon = (props: Props) => {
+    return (
+        <svg className="icon">
+            <use xlinkHref={'#' + props.name}/>
+        </svg>
+    )
+}
+
+export default Icon
