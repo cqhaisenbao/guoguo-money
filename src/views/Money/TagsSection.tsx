664b0bf5d76911404421e22f9a12c5e7f518a3c6
cqhaisenbao <fanyu9353@gmail.com>
2020-11-11 10:21:10 +0800

    重构：将tag:string 重构为tag:{id:number;name:string}

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -15,34 +15,35 @@ const Wrapper = styled.section`
 
 // 声明TagsSection是一个React函数组件；除了接受默认的children，还接受一个selected，类型是string[]
 type Props = {
-    value: string[] ;
-    onChange:(selected:string[])=>void;  /*函数的参数和返回值都为空*/
+    value: number[];
+    onChange: (selected: number[]) => void;  /*函数的参数和返回值都为空*/
 }
 const TagsSection: React.FunctionComponent<Props> = (props) => {
     const {tags, setTags} = useTags();
-    const selectedTags=props.value;
+    const selectedTagIds = props.value;
     const onAddTag = () => {
         const tagName = window.prompt('新标签的名称为');
         if (tagName !== null) {
-            setTags([...tags, tagName])
+            setTags([...tags, {id: Math.random(), name: tagName}])
         }
     };
-    const onToggleTag = (tag: string) => {
-        const index = selectedTags.indexOf(tag);
+    const onToggleTag = (tagId: number) => {
+        const index = selectedTagIds.indexOf(tagId);
         if (index >= 0) {
-            props.onChange(selectedTags.filter(t => t !== tag))
+            props.onChange(selectedTagIds.filter(t => t !== tagId))
         } else {
-            props.onChange([...selectedTags, tag])
+            props.onChange([...selectedTagIds, tagId])
         }
     };
+    const getClass=(tagId:number)=>selectedTagIds.indexOf(tagId)>=0? 'selected' : '';
     return (
         <Wrapper>
             <ol>
                 {tags.map(tag =>
-                    <li key={tag} onClick={() => {
+                    <li key={tag.id} onClick={() => {
                         // 把tag参数传给onToggleTag，传参必须这样写，如果onClick=onToggleTag(tag)会在当时就执行，返回undefined。总结：onClick等于一个可以执行的函数而不是函数返回值，想传参又不执行，箭头函数包起来
-                        onToggleTag(tag)
-                    }} className={selectedTags.indexOf(tag) >= 0 ? 'selected' : ''}>{tag}</li>
+                        onToggleTag(tag.id)
+                    }} className={getClass(tag.id)}>{tag.name}</li>
                 )}
             </ol>
             <button onClick={onAddTag}>新增标签</button>
--- src/views/useTags.tsx
+++ src/views/useTags.tsx
@@ -2,8 +2,14 @@ import {useState} from "react";
 
 //封装一个自定义Hook：用use开头就是自定义Hook，在自定义函数里面使用useState等等，然后把读写接口放到外面去
 const useTags = () => {
-    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
-    return{
+    const [tags, setTags] = useState<{ id: number; name: string }[]>
+    ([
+        {id: 1, name: '衣'},
+        {id: 2, name: '食'},
+        {id: 3, name: '住'},
+        {id: 4, name: '行'},
+    ]);
+    return {
         tags,
         setTags
     }
